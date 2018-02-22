4f47352c23767518dd4434fc814438ebd5016b10
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-02-22 08:14:02 -0300

    se hace funcion de imagen del dia

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
+window.onload = function () {
+  window.nasaConnection = new nasaConnection();
+  $('#askNews').click(getNews);
+
+  let navImages = document.getElementById('navImages');
+
+
+};
+
 // -------------------------------Initializes nasaConnection.
 function nasaConnection() {
   this.checkSetup();
@@ -10,7 +19,7 @@ function nasaConnection() {
   this.signInSnackbar = document.getElementById('must-signin');
   this.signInContainer = document.getElementById('signInContainer');
   this.dailyImgContainer = document.getElementById('dailyImgContainer');
-  
+
   this.newsContainer = document.getElementById('newsContainer');
 
   this.signOutButton.addEventListener('click', this.signOut.bind(this));
@@ -21,7 +30,7 @@ function nasaConnection() {
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
 
-nasaConnection.prototype.initFirebase = function() {
+nasaConnection.prototype.initFirebase = function () {
   // Shortcuts to Firebase SDK features.
   this.auth = firebase.auth();
   this.database = firebase.database();
@@ -31,7 +40,7 @@ nasaConnection.prototype.initFirebase = function() {
 };
 
 
-nasaConnection.prototype.signIn = function() {
+nasaConnection.prototype.signIn = function () {
   // Sign in Firebase using popup auth and Google as the identity provider.
   var provider = new firebase.auth.GoogleAuthProvider();
   this.auth.signInWithPopup(provider);
@@ -39,14 +48,14 @@ nasaConnection.prototype.signIn = function() {
 
 // Signs-out of Friendly Chat.
 
-nasaConnection.prototype.signOut = function() {
+nasaConnection.prototype.signOut = function () {
   // Sign out of Firebase.
   this.auth.signOut();
 };
 
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 
-nasaConnection.prototype.onAuthStateChanged = function(user) {
+nasaConnection.prototype.onAuthStateChanged = function (user) {
   if (user) { // User is signed in!
     // Get profile pic and user's name from the Firebase user object.
     var profilePicUrl = user.photoURL; // Only change these two lines!
@@ -63,7 +72,7 @@ nasaConnection.prototype.onAuthStateChanged = function(user) {
 
     // Hide sign-in button.
     this.signInContainer.setAttribute('hidden', 'true');
-    this.dailyImgContainer.removeAttribute('hidden');
+    // this.dailyImgContainer.removeAttribute('hidden');
     this.newsContainer.removeAttribute('hidden');
   } else { // User is signed out!
     // Hide user's profile and sign-out button.
@@ -73,13 +82,13 @@ nasaConnection.prototype.onAuthStateChanged = function(user) {
 
     // Show sign-in button.
     this.signInContainer.removeAttribute('hidden');
-    this.dailyImgContainer.setAttribute('hidden', 'true');
+    // this.dailyImgContainer.setAttribute('hidden', 'true');
     this.newsContainer.setAttribute('hidden', 'true');
   }
 };
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
-nasaConnection.prototype.checkSignedInWithMessage = function() {
+nasaConnection.prototype.checkSignedInWithMessage = function () {
   // Return true if the user is signed in Firebase
   if (this.auth.currentUser) {
     return true;
@@ -95,7 +104,7 @@ nasaConnection.prototype.checkSignedInWithMessage = function() {
 
 
 // Checks that the Firebase SDK has been correctly setup and configured.
-nasaConnection.prototype.checkSetup = function() {
+nasaConnection.prototype.checkSetup = function () {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
       'Make sure you go through the codelab setup instructions and make ' +
@@ -103,14 +112,10 @@ nasaConnection.prototype.checkSetup = function() {
   }
 };
 
-window.onload = function() {
-  window.nasaConnection = new nasaConnection();
-};
-
 // buscar por https://nasa-project-nienna.firebaseapp.com
 // url (requerida), opciones (opcional) SBmiPgE8WXMZztwJFFZhKpAb5CyIKhGr8kUTD5zz   https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY 
 
-let btnSearchPlanet = document.getElementById('btnSearchPlanet');
+
 
 function showApiNAsa() {
   $('#images').empty();
@@ -120,96 +125,187 @@ function showApiNAsa() {
   }).then(function (response) {
     response.json()
       .then(function (response) {
-        console.log(response);
+        // console.log(response);
         let images = response.collection.items;
         let descript = response.collection.items;
         images.filter(element => {
           let imageHref = element.links[0].href;
           let description = element.data[0].description_508;
-          $('#images').append(`<div class="col col-lg-4" >
-          <div class="img-thumbnail" >
-            <a href="#">
-            <img src="${imageHref}" alt="planets" class="imgSearch">
-            <div class="caption">
-            <p>${description}</p>
-            </div>
-            </a>
-            </div>
-          </div>`);
+
+          if (imageHref !== undefined && description !== undefined) {
+            $('#images').append(`<div class="col col-lg-4" >
+            <div class="img-thumbnail imgdiv" >
+             <a href="#">
+             <img src="${imageHref}" alt="planets" class="imgSearch imgNews img-responsive">
+             <div class="text-center" >
+             <p class="caption">${description}</p>
+             </div>
+             </a>
+             </div>
+             </div>`);
+          }
+
         });
       });
   }).catch(function (err) {
     // Error :(
   });//https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&begin=2014-02-01&api_key=DEMO_KEY
 }//https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
-btnSearchPlanet.addEventListener('click', showApiNAsa);
 
 // ------------------------------------------- NEWS API------
 
-function getNews() {
+function loadNews() {
   let searchWord = document.getElementById('searchWord').value;
   let sortBy = document.getElementById('sortBy').value;
   let language = document.getElementById('language').value;
   let news = document.getElementById('news');
+  let askNews = document.getElementById('askNews');
 
   // fetch('https://newsapi.org/v2/top-headlines?apiKey=19214f11097341d1ad450bb2ad214ce1&country=us&category=science&q=nasa')
   fetch(`https://newsapi.org/v2/everything?apiKey=19214f11097341d1ad450bb2ad214ce1&q=nasa%20science&language=${language}&sortBy${sortBy}`)
-    .then(function(response) {
+    .then(function (response) {
       // Turns the the JSON into a JS object
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
       console.log(data.articles[1]);
       for (let i = 0; i < data.articles.length; i++) {
-      // Let's make some HTML!
-        
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'newDiv');
-
-        let newImg = document.createElement('img');
-        newImg.setAttribute('class', 'imgNews img-fluid');
-        newImg.setAttribute('src', data.articles[i].urlToImage);
-
-        let newTitle = document.createElement('h3');
-        newTitle.setAttribute('class', 'titleNews');
-        let newHref = document.createElement('a');
-        newHref.setAttribute('class', 'urlNews');
-        newHref.setAttribute('href', data.articles[i].url);
-        newTitle.appendChild(newHref);
-
-        let newDescription = document.createElement('p');
-        newDescription.setAttribute('class', 'bodyNews');
-        let newText = document.createTextNode(data.articles[i].description);
-        newDescription.appendChild(newText);
-
-        let newDate = document.createElement('h5');
-        newDate.setAttribute('class', 'dates');
-        let textDate = document.createTextNode('Date: ' + data.articles[i].publishedAt);
-        newDate.appendChild(textDate);
-
-        let newSource = document.createElement('h5');
-        newSource.setAttribute('class', 'sourceNews');
-        let textSource = document.createTextNode(data.articles[i].source.name);
-        newSource.appendChild(textSource);
-
-        newDiv.appendChild(newImg);
-        newDiv.appendChild(newTitle);
-        newDiv.appendChild(newDescription);
-        newDiv.appendChild(newDate);
-        // newDiv.appendChild(newSource);
-
-        news.append(newDiv);
+        let info = data.articles[i];
+        let infoImg = info.urlToImage;
+        console.log(infoImg);
+        if (info.urlToImage === null || infoImg.indexOf('https') < 0) {
+          let newDiv = `<div class="row newDiv">
+                        <div class="col-lg-12">
+                          <h3 class="titleNews"><a href="${info.url}" class="urlNews">${info.title}</a></h3>
+                          <h5 class="descriptioNres">${info.description}</h5>
+                          <h6 class="dateNews">Date: ${info.publishedAt}.</h6>        
+                          <h6 class="sourceNews"> Publicado en: ${info.source.name}</h6>
+                        </div>
+                      </div>`;
+          $('#news').append(newDiv);
+        } else {
+          let newDiv = `<div class="row newDiv">
+                        <div class="col-lg-12 text-center">
+                        <img src="${info.urlToImage}" class="imgNews rounded mx-auto d-block img-fluid" alt="">
+                          <h3 class="titleNews"><a href="${info.url}" class="urlNews">${info.title}</a></h3>
+                          <h5 class="descriptioNres">${info.description}</h5>
+                          <h6 class="dateNews">Date: ${info.publishedAt}.</h6>        
+                          <h6 class="sourceNews"> Publicado en: ${info.source.name}</h6>
+                        </div>
+                      </div>`;
+          $('#news').append(newDiv);
+          ;
+        }
       };
     })
-    .catch(function(error) {
-      if (searchWord === null || sortBy === null || language === null) {
-        console.log('Something not found');
-      }
+    .catch(function (error) {
+      console.log('Something not found');
     });
 }
+loadNews();
 
-getNews();
+function getNews() {
+  console.log('whaaaaat');
+  $('#news').empty();
+  let searchWord = document.getElementById('searchWord').value;
+  let sortBy = document.getElementById('sortBy').value;
+  let language = document.getElementById('language').value;
+  let news = document.getElementById('news');
+  let askNews = document.getElementById('askNews');
 
+  // fetch('https://newsapi.org/v2/top-headlines?apiKey=19214f11097341d1ad450bb2ad214ce1&country=us&category=science&q=nasa')
+  fetch(`https://newsapi.org/v2/everything?apiKey=19214f11097341d1ad450bb2ad214ce1&q=nasa%20science%20${searchWord}&language=${language}&sortBy${sortBy}`)
+    .then(function (response) {
+      // Turns the the JSON into a JS object
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      // console.log(data.articles[1]);
+      for (let i = 0; i < data.articles.length; i++) {
+        let info = data.articles[i];
+        let infoImg = info.urlToImage;
+        // console.log(infoImg);
+        if (infoImg === null || infoImg.indexOf('https') < 0) {
+          let newDiv = `<div class="row newDiv">
+                        <div class="col-lg-18">
+                          <h3 class="titleNews"><a href="${info.url}" class="urlNews">${info.title}</a></h3>
+                          <h5 class="descriptioNres">${info.description}</h5>
+                          <h6 class="dateNews">Date: ${info.publishedAt}.</h6>        
+                          <h6 class="sourceNews"> Publicado en: ${info.source.name}</h6>
+                        </div>
+                      </div>`;
+          $('#news').append(newDiv);
+        } else {
+          let newDiv = `<div class="row newDiv">
+                        <div class="col-lg-12 text-center">
+                        <img src="${info.urlToImage}" class="imgNews rounded mx-auto d-block img-fluid" alt="">
+                          <h3 class="titleNews"><a href="${info.url}" class="urlNews">${info.title}</a></h3>
+                          <h5 class="descriptioNres">${info.description}</h5>
+                          <h6 class="dateNews">Date: ${info.publishedAt}.</h6>        
+                          <h6 class="sourceNews"> Publicado en: ${info.source.name}</h6>
+                        </div>
+                      </div>`;
+          $('#news').append(newDiv);
+          ;
+        }
+      };
+    })
+    .catch(function (error) {
+      console.log('Something not found');
+    });
+};
+
+// image of the day
+(function day() {
+  fetch('https://api.nasa.gov/planetary/apod?api_key=SBmiPgE8WXMZztwJFFZhKpAb5CyIKhGr8kUTD5zz').then(function (response) {
+    return response.json();
+  }).then(function (response) {
+    console.log(response);
+    let data = [response];
+    data.filter(element => {
+      let img = element.url;
+      let title = element.title;
+      let description = element.explanation;
+      let date = element.date;
+      $('#template').append(`<section class="container">
+                              <div class="row">
+                                <div class="col-lg-12">
+                                  <div class="jumbotron jumbotron-fluid">
+                                    <div class="container">
+                                    <h1 class="display-4 text-center titleday">Image of the day</h1>
+                                    <img src="${img}" alt="imgDay" class="img-responsive mx-auto imgDay">
+                                    <hr class="my-4">
+                                    <h3 class="display-5">${title}</h3>
+                                    <h4 class="display-6">${date}</h4>
+                                    <p>${description}</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </section>`);
+    });
+  });
+})();
+// searching images
+let showImages = () => {
+  $('#template').empty();
+  $('#template').append(`<section class="container" id="showTheImages">
+                    <div class="row">
+                    <div class="col col-lg-12">
+                    <h2 class="title2">Search Images</h2>
+                    <div class="askNav text-center">
+                    <input type="text" id="searchPlanet" class="form-control">
+                    <button class="btn btn-primary" id="btnSearchPlanet">Buscar</button>
+                    </div
+                    </div>
+                    </div>
+                    <div class="row" id="images"></div>
+                    </section>`);
+
+  $('#btnSearchPlanet').click(showApiNAsa);
+
+};
+navImages.addEventListener('click', showImages);
 // -----------------------------------------------FIN NEWS API
-
